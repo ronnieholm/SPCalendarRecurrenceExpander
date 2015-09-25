8e2023e5672ab70b211f5cee66e4881080e11021
ronnieholm <mail@bugfree.dk>
2015-09-25 23:58:52 +0200

    Bump version number for NuGet publishing

--- Src/SPCalendarRecurrenceExpander/AssemblyInfo.fs
+++ Src/SPCalendarRecurrenceExpander/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Reflection
 [<assembly: AssemblyTitle("SPCalendarRecurrenceExpander")>]
 [<assembly: AssemblyDescription("Turns SharePoint calendar recurrence events into a series of individual events, taking into account recurrence exceptions.")>]
 [<assembly: AssemblyCopyright("Copyright (c) 2014-2015 Ronnie Holm <mail@bugfree.dk>")>]
-[<assembly: AssemblyVersion("1.0.1")>]
-[<assembly: AssemblyFileVersion("1.0.1")>]
+[<assembly: AssemblyVersion("1.0.2")>]
+[<assembly: AssemblyFileVersion("1.0.2")>]
 
 do ()
